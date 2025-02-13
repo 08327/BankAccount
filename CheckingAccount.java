84e197c6f4476fcedbcee1a323a720828a7fdd03
sean <114786275+08327@users.noreply.github.com>
2025-02-13 15:22:02 -0500

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,2 +1,10 @@
-public class CheckingAccount {
+public class CheckingAccount extends BankAccount{
+    private int FREE_TRANSACTIONS = 3;
+    private double TRANSACTION_FEE = 2.0;
+    private int transactions;
+
+    public CheckingAccount(double balance){
+        super(balance);
+        transactions = 0;
+    }
 }
