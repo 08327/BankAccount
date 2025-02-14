1095fe2e28dc80a9304c14f212a24938744c5a69
sean <114786275+08327@users.noreply.github.com>
2025-02-13 20:51:34 -0500

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,2 +1,18 @@
-public class SavingsAccount {
+public class SavingsAccount extends BankAccount{
+    private double interestRate;
+
+    public SavingsAccount(double balance, double interestRate) {
+        //use super to get balance from bank account
+        super(balance);
+        this.interestRate = interestRate;
+    }
+
+    // get the total out put with interestRate and Balance
+    public void addInterest(){
+        deposit(getBalance() * interestRate);
+    }
+
+    public String toString() {
+        return super.toString() + ", interestRate="+interestRate;
+    }
 }
